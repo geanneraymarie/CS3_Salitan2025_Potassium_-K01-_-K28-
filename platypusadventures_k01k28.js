de9c61f25d52d323a7bb1925beb713ff633af96d
maggie-velasco <b2029jmbvelasco@pshs.edu.ph>
2025-11-27 14:33:56 +0800

    Update platypusadventures_k01k28.js

--- platypusadventures_k01k28.js
+++ platypusadventures_k01k28.js
@@ -1,1 +1,40 @@
      
+function ourFunction() {
+     function problem1() {
+        const cases = Math.floor((Math.random()*4) + 1);
+        document.write("To get the 4-digit pin, you must solve the following riddle:");
+        switch(cases) {
+            case 1:
+                alert("");
+            break;
+
+            case 2:
+                alert("");
+            break;
+
+            case 3:
+                alert("");
+            break;
+            
+            case 4:
+                alert("");
+            break;
+
+        }
+        //answers:
+        //6242
+        //0316
+        //4671
+        //7777
+        var textboxy;
+
+
+     }
+     function problem2() {
+
+     }
+     function problem3() {
+
+     }
+}
+
