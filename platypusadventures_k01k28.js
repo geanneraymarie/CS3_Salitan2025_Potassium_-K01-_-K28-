2895fc581258af567997de0f80f6118d9659267e
maggie-velasco <b2029jmbvelasco@pshs.edu.ph>
2025-12-01 13:49:21 +0800

    Update platypusadventures_k01k28.js

--- platypusadventures_k01k28.js
+++ platypusadventures_k01k28.js
@@ -1,23 +1,69 @@
-     
 function ourFunction() {
      function problem1() {
+        let outputHEH;
+        
         const cases = Math.floor((Math.random()*4) + 1);
         document.write("To get the 4-digit pin, you must solve the following riddle:");
         switch(cases) {
             case 1:
-                alert("");
+                let problem1 = prompt("What is the password?", "0000");
+                if (problem1 == null || problem1 == "") {
+                    outputHEH = "Enter the fricking password!";
+                }
+                else {
+                    if(problem1 == "6242") {
+                        outputHEH = "Correct! >>proceeds to program";
+                    }
+                    else {
+                        outputHEH = "Incorrect.Try again";
+                    }
+                }
+
             break;
 
             case 2:
-                alert("");
+                let problem2 = prompt("What is the password?", "0000");
+                if (problem2 == null || problem2 == "") {
+                    outputHEH = "Enter the fricking password!";
+                }
+                else {
+                    if(problem2 == "0316") {
+                        outputHEH = "Correct! >>proceeds to program";
+                    }
+                    else {
+                        outputHEH = "Incorrect.Try again";
+                    }
+                }
             break;
 
             case 3:
-                alert("");
+                let problem3 = prompt("What is the password?", "0000");
+                if (problem3 == null || problem3 == "") {
+                    outputHEH = "Enter the fricking password!";
+                }
+                else {
+                    if(problem3 == "4671") {
+                        outputHEH = "Correct! >>proceeds to program";
+                    }
+                    else {
+                        outputHEH = "Incorrect.Try again";
+                    }
+                }
             break;
             
             case 4:
-                alert("");
+                let problem4 = prompt("What is the password?", "0000");
+                if (problem4 == null || problem4 == "") {
+                    outputHEH = "Enter the fricking password!";
+                }
+                else {
+                    if(problem4 == "7777") {
+                        outputHEH = "Correct! >>proceeds to program";
+                    }
+                    else {
+                        outputHEH = "Incorrect.Try again";
+                    }
+                }
             break;
 
         }
@@ -27,14 +73,29 @@ function ourFunction() {
         //4671
         //7777
         var textboxy;
+        document.getElementById("button").innerHTML = outputHEH;
 
 
      }
      function problem2() {
-
+        
      }
      function problem3() {
 
      }
 }
 
+
+/*rli = require('readline').createInterface({
+    input : process.stdin,
+}) 
+rli.on('line', function (line) {
+    line !=="END" ? inputs.push(line) : rli.close()
+})
+
+var inputs = []
+
+rli.on('close', ourFunction)*/
+
+
+  
