ea7b98c5ba3a77a048be51dfed62ea8866186abf
maggie-velasco <b2029jmbvelasco@pshs.edu.ph>
2025-12-06 21:28:09 +0800

    Create platypusadventures_k01k28p3.js

---
+++ platypusadventures_k01k28p3.js
@@ -0,0 +1,64 @@
+function problem3() {
+          let lastOutput;
+        
+          const cases3 = Math.floor((Math.random()*4) + 1);
+          switch(cases3) {
+          case 1:
+               let problem31 = prompt("");
+               if (problem31 == null || problem31 == "") {
+                    document.write("Enter the fricking password!");
+               }
+               else {
+                    if(problem31 == "4671") {
+                         document.write("Correct! >>proceeds to program");
+                    }
+                    else {
+                         document.write("Incorrect.Try again");
+                    }
+               }
+               break;
+          case 2:
+               let problem32 = prompt("");
+               if (problem32 == null || problem32 == "") {
+                    document.write("Enter the fricking password!");
+               }
+               else {
+                    if(problem32 == "4671") {
+                         document.write("Correct! >>proceeds to program");
+                    }
+                    else {
+                         document.write("Incorrect.Try again");
+                    }
+               }
+               break;
+          case 3:
+               let problem33 = prompt("");
+               if (problem33 == null || problem33 == "") {
+                    document.write("Enter the fricking password!");
+               }
+               else {
+                    if(problem33 == "4671") {
+                         document.write("Correct! >>proceeds to program");
+                    }
+                    else {
+                         document.write("Incorrect.Try again");
+                    }
+               }
+               break;
+          case 4:
+               let problem34 = prompt("");
+               if (problem34 == null || problem34 == "") {
+                    document.write("Enter the fricking password!");
+               }
+               else {
+                    if(problem34 == "4671") {
+                         document.write("Correct! >>proceeds to program");
+                    }
+                    else {
+                         document.write("Incorrect.Try again");
+                    }
+               }
+               break;
+               
+     }
+     }
